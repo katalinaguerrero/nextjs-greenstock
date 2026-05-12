d4f5e595da220c749b88b76e0218cd2e85199c0e
Katalina <katest2599@gmail.com>
2026-05-12 15:34:53 -0400

    chore: set dynamic rendering and revalidation for PlantsPage

--- app/plants/page.tsx
+++ app/plants/page.tsx
@@ -1,6 +1,9 @@
 import { plantService } from "@/services/plantService";
 import PlantsTable from "@/components/plant/PlantsTable";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function PlantsPage() {
   const plants = await plantService.getPlants();
 
